7534ec5d00bf7a98093eef51786cf9c4d0c9c343
karanbhatia26 <karanbhatia261204@gmail.com>
2024-02-19 20:21:10 +0530

    blogs page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,23 +1,22 @@
 import { auth } from "../components/firebase-config";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
-  // let navigate = useNavigate();
-  
+function Login({ setIsAuth }) {
+  let navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
 
-  const signInWithGoogle = async() => {
-    const provider = new GoogleAuthProvider();
+  const signInWithGoogle = () => {
     signInWithPopup(auth, provider)
-      // .then((result) => {
-      //   localStorage.setItem("isAuth", true);
-      //   setIsAuth(true);
-      //   navigate("/");
-      // })
-      // .catch((error) => {
-      //   // Handle sign-in errors here
-      //   console.error("Error signing in with Google:", error);
-      // });
+      .then((result) => {
+        localStorage.setItem("isAuth", true);
+        setIsAuth(true);
+        navigate("/");
+      })
+      .catch((error) => {
+        // Handle sign-in errors here
+        console.error("Error signing in with Google:", error);
+      });
   };
 
   return (
