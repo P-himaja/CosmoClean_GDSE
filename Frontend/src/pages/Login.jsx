f85568a15ec49457c12f3e973e2b98c3e234df2f
P-himaja <himajapannati29@gmail.com>
2024-02-20 00:15:18 +0530

    login page added

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -20,13 +20,43 @@ function Login({ setIsAuth }) {
   };
 
   return (
-    <div className="loginPage">
-      <p>Sign In With Google to Continue</p>
-      <button className="login-with-google-btn" onClick={signInWithGoogle}>
-        Sign in with Google
-      </button>
+    <div className="login_back">
+    <div className="Sign_container">
+      <div className="Sign_full_box">
+        <div className="Sign_form-container">
+          <form action="user_login.php" method="post">
+            <h2>Sign In</h2>
+            <input type="text" placeholder="Username or Email" name="uname" id="username" required />
+            <input type="password" placeholder="Password" name="psw" id="password" required />
+            <button type="submit" id="sign-up">Sign In</button>
+          </form>
+        </div>
+
+        <div className="Sign_google">
+          <p>OR</p>
+          <div className="Sign_signin">
+            <button type="button" className="login-with-google-btn" id="googlee" onClick={signInWithGoogle}>
+              Sign in with Google
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="Sign_title">
+        <span>Welcome to </span>
+        <b id="main_login">CosmoClean!</b>
+      </div>
+    </div>
     </div>
   );
 }
-
 export default Login;
+// return (
+//   <div className="loginPage">
+//     <p>Sign In With Google to Continue</p>
+//     <button className="login-with-google-btn" onClick={signInWithGoogle}>
+//       Sign in with Google
+//     </button>
+//   </div>
+// );
+// }
--- Frontend/src/pages/Research.jsx
+++ Frontend/src/pages/Research.jsx
@@ -6,7 +6,7 @@ import graphC from '../images/graph3.png';
 import analytics from '../images/analysis.png';
 
 const options = [
-  // { value: "all", label: "All" },
+  
   { value: "option1", label: "Option 1" },
   { value: 'option2', label: 'Option 2' },
   { value: 'option3', label: 'Option 3' },
